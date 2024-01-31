aa300469345bf267d8b9dc0b4d345e28a8b15142
Samuelbrewer58 <sam.brewer13@yahoo.com>
2024-01-31 13:14:53 -0700

    custom types

--- app.ts
+++ app.ts
@@ -1,21 +1,31 @@
-function combine(input1: number | string, input2: number | string, resultConversion: "as-number" | "as-text") {
-	let result;
-	if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
-		result = +input1 + +input2;
-	} else {
-		result = input1.toString() + input2.toString();
-	}
-	return result;
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
 
-	// if (resultConversion === "as-number") {
-	// 	return +result;
-	// } else {
-	// 	return result.toString();
-	// }
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  let result;
+  if (
+    (typeof input1 === 'number' && typeof input2 === 'number') ||
+    resultConversion === 'as-number'
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+
+  // if (resultConversion === "as-number") {
+  // 	return +result;
+  // } else {
+  // 	return result.toString();
+  // }
 }
 
-const combinedAges = combine(30, 26, "as-number");
+const combinedAges = combine(30, 26, 'as-number');
 console.log(combinedAges);
 
-const combinedNames = combine("Samuel", "Samantha", "as-text");
+const combinedNames = combine('Samuel', 'Samantha', 'as-text');
 console.log(combinedNames);
